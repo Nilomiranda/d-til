c82fd2f7542fcf9c4404763edc3370fa9852ef92
Nilomiranda <nilomiranda3@gmail.com>
2022-01-24 14:52:43 -0300

    exporting allowed folders

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const githubUserName = import.meta.env.VITE_GITHUB_USERNAME;
 export const repositoryName = import.meta.env.VITE_GITHUB_REPOSITORY;
+export const allowedFolderNames = ['html', 'mongo'];
