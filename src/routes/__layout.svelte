c9d56f263f0871b80d950b475dc9519c77227429
Nilomiranda <nilomiranda3@gmail.com>
2022-01-24 15:04:25 -0300

    added footer about free icon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,8 +17,11 @@
 <Header />
 
 <QueryClientProvider client={queryClient}>
-	<main>
+	<main class="h-full">
 		<slot />
 	</main>	
+    <footer class="w-full flex justify-center">
+        <a href="https://www.flaticon.com/free-icons/notes" title="notes icons">Notes icons created by Freepik - Flaticon</a>
+    </footer>
 </QueryClientProvider>
 
