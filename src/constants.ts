c07d449b54ccc61372b7e1a02fb2106bff87d715
Nilomiranda <nilomiranda3@gmail.com>
2022-01-26 14:04:18 -0300

    allowing eslint folder

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const githubUserName = import.meta.env.VITE_GITHUB_USERNAME;
 export const repositoryName = import.meta.env.VITE_GITHUB_REPOSITORY;
-export const allowedFolderNames = ['html', 'mongo'];
+export const allowedFolderNames = ['html', 'mongo', 'eslint'];
